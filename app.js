b9d06b0521ad2f298978f26062a4daea22674563
axschech <ax.schech@gmail.com>
2015-10-25 23:55:45 -0400

    adding registration

--- app.js
+++ app.js
@@ -3,19 +3,42 @@ angular.module('app', [
 ])
     .controller('RegisterCtrl', function (
         $scope,
-        Organizations
+        Organizations,
+        User
     ) {
         'use strict';
+        $scope.user = {};
         $scope.organizations = [];
         Organizations.resource.get().$promise.then(function (response) {
             $scope.organizations = response.data ;
             $scope.organizations.unshift({name: 'Please choose an organiztion'});
-            $scope.organization = $scope.organizations[0];
+            $scope.user.organization = $scope.organizations[0].id;
         });
+
+        $scope.register = function () {
+            $scope.warning = "";
+            User.resource
+                .save($scope.user)
+                .$promise
+                .then(function (response) {
+                    console.log(response.data);
+                }, function (response) {
+                    console.log(response.data.error);
+                    if (response.data.error) {
+                        $scope.warning = response.data.error;
+                    }
+                });
+        };
     })
     .factory('Organizations', function ($resource) {
         'use strict';
         return {
             resource: $resource('/got_awareness_node/api/organizations')
+        };
+    })
+    .factory('User', function ($resource) {
+        'use strict';
+        return {
+            resource: $resource('/got_awareness_node/api/users/:user_id')
         };
     });
